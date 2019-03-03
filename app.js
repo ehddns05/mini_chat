ed1ac7004783c2a7b64898d3ba52a902ba72034a
DESKTOP-G475UIG\LG <dongwun05@gmail.com>
2019-03-03 15:57:44 +0900

    set up chat

--- app.js
+++ app.js
@@ -1,48 +1,74 @@
-//Node.js 기본 내장 모듈 로드
-const fs = require('fs');
-//express 모듈 로드
-const express = require('express');
-//socket 모듈 로드
-const socket = require('socket.io');
-//node.js 기본 내장 모듈 로드
-const http = require('http');
-//express객체 생성
-const app = express();
-//express http서버 생성
-const server = http.createServer(app);
-//생성된 서버를 socket.io에 바인딩
-const io = socket(server);
+/* 설치한 express 모듈 불러오기 */
+const express = require('express')
 
-//app.use를 통하여 미들웨어를 추가하여 조합할 수 있다.
+/* 설치한 socket.io 모듈 불러오기 */
+const socket = require('socket.io')
+
+/* Node.js 기본 내장 모듈 불러오기 */
+const http = require('http')
+
+/* Node.js 기본 내장 모듈 불러오기 */
+const fs = require('fs')
+
+/* express 객체 생성 */
+const app = express()
+
+/* express http 서버 생성 */
+const server = http.createServer(app)
+
+/* 생성된 서버를 socket.io에 바인딩 */
+const io = socket(server)
+
 app.use('/css', express.static('./static/css'))
 app.use('/js', express.static('./static/js'))
 
-//서버를 8080포트로 리스너 호출
-server.listen(8080, function(){
-    console.log("Server boot");
-})
-//get 방식으로 /경로에 접속
+/* Get 방식으로 / 경로에 접속하면 실행 됨 */
 app.get('/', function(request, response) {
-    fs.readFile('./static/index.html', function(err, data) {
-      if(err) {
-        response.send('에러')
-      } else {
-        response.writeHead(200, {'Content-Type':'text/html'})
-        response.write(data)
-        response.end()
-      }
-    })
+  fs.readFile('./static/index.html', function(err, data) {
+    if(err) {
+      response.send('에러')
+    } else {
+      response.writeHead(200, {'Content-Type':'text/html'})
+      response.write(data)
+      response.end()
+    }
+  })
+})
+
+io.sockets.on('connection', function(socket) {
+
+  /* 새로운 유저가 접속했을 경우 다른 소켓에게도 알려줌 */
+  socket.on('newUser', function(name) {
+    console.log(name + ' 님이 접속하였습니다.')
+
+    /* 소켓에 이름 저장해두기 */
+    socket.name = name
+
+    /* 모든 소켓에게 전송 */
+    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'})
   })
 
-//on은 소켓에서 해당 이벤트를 받으면 콜백함수가 실행된다.
-io.sockets.on('connection', function(socket) {
-    console.log('connected');
+  /* 전송한 메시지 받기 */
+  socket.on('message', function(data) {
+    /* 받은 데이터에 누가 보냈는지 이름을 추가 */
+    data.name = socket.name
+    
+    console.log(data)
 
-    socket.on('send', function(data)  {
-        console.log('Message : ', data.msg);
-    })
+    /* 보낸 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit('update', data);
+  })
 
-    socket.on('disconnect', function() {
-        console.log('logout');
-    })
+  /* 접속 종료 */
+  socket.on('disconnect', function() {
+    console.log(socket.name + '님이 나가셨습니다.')
+
+    /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
+    socket.broadcast.emit('update', {type: 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
+  })
+})
+
+/* 서버를 8080 포트로 listen */
+server.listen(8888, function() {
+  console.log('서버 실행 중..')
 })
